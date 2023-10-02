b1feec98704c4d5b8a19dd96bdc6737ae20ae6c6
ZachHuff01 <133275000+ZachHuff01@users.noreply.github.com>
2023-10-02 00:10:57 -0400

    debugging

--- model.js
+++ model.js
@@ -26,7 +26,7 @@ let userSchema = mongoose.Schema({
 });
 
 userSchema.statics.hashPassword = (password) = {
-    return: bcrypt.hashSync(password, 10),
+    return: bcrypt.hashSync(hashPassword, 10),
 };
 
 userSchema.methods.validatePassword = function(password){
