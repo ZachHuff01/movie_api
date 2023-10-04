77dc6be4fb0a0ad0ac21a88a3811008b87d4c888
ZachHuff01 <133275000+ZachHuff01@users.noreply.github.com>
2023-10-04 02:48:33 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const {check, validationResult} = require('express-validator');
 //   'mongodb://localhost:27017/myDB', 
 //   {useNewUrlParser: true, useUnifiedTopology: true }
 //   );
-  mongoose.connect(process.env.CONNECTION_URI, {
+  mongoose.connect('mongodb+srv://zhuff1:VDimYzoPO8iGKF1x@mydb.tzivuue.mongodb.net/myDB?retryWrites=true&w=majority', {
     useNewUrlParser: true, 
     useUnifiedTopology: true,
   });
