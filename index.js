d39c825d5de579e8445664efce60fec1d37c86fc
ZachHuff01 <133275000+ZachHuff01@users.noreply.github.com>
2023-10-08 09:33:24 -0400

    update

--- index.js
+++ index.js
@@ -36,16 +36,16 @@ app.use(cors());
 //Allows Certain domains to use API
 let allowedOrigins = ['http://localhost:8080', 'http://testsite.com'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application does not allow access from origin ' + origin;
-      return callback(new Error(message ), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if(!origin) return callback(null, true);
+//     if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+//       return callback(new Error(message ), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 // Authentification & Login Endpoint
 let auth = require('./auth')(app) // Login HTML Authentification
